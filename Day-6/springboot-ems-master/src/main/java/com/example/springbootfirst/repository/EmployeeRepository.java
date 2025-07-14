e77fbf93ee747c3f332bea595eb3ae67539e5d64
kamalesh-G-J <kamalesh.gj2023ai-ds@sece.ac.in>
2025-07-14 17:14:12 +0530

    Deploy_Springboot

--- Day-6/springboot-ems-master/src/main/java/com/example/springbootfirst/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootfirst.repository;
-
-import com.example.springbootfirst.models.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
---
+++ src/main/java/com/example/springbootfirst/controllers/Hello.java
@@ -0,0 +1,9 @@
+package com.example.springbootfirst.controllers;
+
+public class Hello {
+
+
+    public String HelloTest(){
+        return "Hello test";
+    }
+}
